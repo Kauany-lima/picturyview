c998ff6688a06cc8848a51cc11f31196a78e8d75
Kauany lima <kauany.lima@estudante.ifro.edu.br>
2023-10-20 19:08:17 -0400

    projeto

---
+++ PictureViewer/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.mostrarimagem = new System.Windows.Forms.Button();
+            this.limpar = new System.Windows.Forms.Button();
+            this.definircor = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.fechar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(848, 578);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(842, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 523);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.mostrarimagem);
+            this.flowLayoutPanel1.Controls.Add(this.limpar);
+            this.flowLayoutPanel1.Controls.Add(this.definircor);
+            this.flowLayoutPanel1.Controls.Add(this.fechar);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(130, 523);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(715, 52);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // mostrarimagem
+            // 
+            this.mostrarimagem.AutoSize = true;
+            this.mostrarimagem.Location = new System.Drawing.Point(557, 3);
+            this.mostrarimagem.Name = "mostrarimagem";
+            this.mostrarimagem.Size = new System.Drawing.Size(155, 40);
+            this.mostrarimagem.TabIndex = 0;
+            this.mostrarimagem.Text = "mostrar imagem";
+            this.mostrarimagem.UseVisualStyleBackColor = true;
+            this.mostrarimagem.Click += new System.EventHandler(this.mostrarimagem_Click);
+            // 
+            // limpar
+            // 
+            this.limpar.AutoSize = true;
+            this.limpar.Location = new System.Drawing.Point(392, 3);
+            this.limpar.Name = "limpar";
+            this.limpar.Size = new System.Drawing.Size(159, 40);
+            this.limpar.TabIndex = 1;
+            this.limpar.Text = "limpar a imagem";
+            this.limpar.UseVisualStyleBackColor = true;
+            this.limpar.Click += new System.EventHandler(this.limpar_Click);
+            // 
+            // definircor
+            // 
+            this.definircor.AutoSize = true;
+            this.definircor.Location = new System.Drawing.Point(173, 3);
+            this.definircor.Name = "definircor";
+            this.definircor.Size = new System.Drawing.Size(213, 40);
+            this.definircor.TabIndex = 2;
+            this.definircor.Text = "Definir a cor da tela de fundo";
+            this.definircor.UseVisualStyleBackColor = true;
+            this.definircor.Click += new System.EventHandler(this.definircor_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(569, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "fechar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // fechar
+            // 
+            this.fechar.AutoSize = true;
+            this.fechar.Location = new System.Drawing.Point(24, 3);
+            this.fechar.Name = "fechar";
+            this.fechar.Size = new System.Drawing.Size(143, 40);
+            this.fechar.TabIndex = 3;
+            this.fechar.Text = "fechar";
+            this.fechar.UseVisualStyleBackColor = true;
+            this.fechar.Click += new System.EventHandler(this.fechar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Title = "Selecionar um arquivo de imagem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 578);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Visualizador de Imagens";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button mostrarimagem;
+        private System.Windows.Forms.Button limpar;
+        private System.Windows.Forms.Button definircor;
+        private System.Windows.Forms.Button fechar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mostrarimagem_Click(object sender, EventArgs e)
+        {
+            //Mostrar a caixa de diálogo Abrir arquivo. Se o usuário clicar em OK, carregue o imagem que o usuário escolheu.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void limpar_Click(object sender, EventArgs e)
+        {
+            
+            
+                // limpar a imagem.
+                pictureBox1.Image = null;
+            
+        }
+
+        private void definircor_Click(object sender, EventArgs e)
+        {
+
+            // Mostrar a caixa de diálogo de cor.Se o usuário clicar em OK, altere o
+            // Plano de fundo do controle PictureBox para a cor que o usuário escolheu.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void fechar_Click(object sender, EventArgs e)
+        {
+            //fechar
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Se o usuário marcar a caixa de seleção Alongar, alterar o PictureBox
+            //SizeMode propriedade para "Stretch".Se o usuário limpar
+            //a caixa de seleção, altere - a para "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
